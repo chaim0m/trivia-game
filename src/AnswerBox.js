9d4acfc8e4751ff440191e4a477ce72acc81818c
Chaim Finson <chaim0m@hotmail.com>
2018-02-12 13:59:31 +0200

    answers accessed

--- src/AnswerBox.js
+++ src/AnswerBox.js
@@ -4,15 +4,28 @@ export default class AnswerBox extends Component {
     constructor(props) {
         super(props);
         this.handleClick = this.handleClick.bind(this)
-        this.shuffle = this.shuffle.bind(this)
+        // this.shuffle = this.shuffle.bind(this)
         this.state = {
-            answers: this.props.item
+            data: this.props
         }
     }
     
     handleClick(e) {
         console.log(e.target);
-        console.log(e.target.value);
+        console.log(e.target.innerHTML);
+        console.log(this.state.data.answers);
+                let pickedAnswer = e.target.innerHTML;
+                let allAnswers = this.state.data.answers;
+        for (let answer of allAnswers ){
+            if (answer.isCorrect){
+                if(answer.value==pickedAnswer){
+                    alert("Great Job! You got the right answer.")
+                }
+                else{
+                    alert("oh no, answer incorrect")
+                }
+            }
+        }
         // if (e.target.props.item.isCorrect){
         //     e.target.className = "hex-option border green"
         // } else {
@@ -46,18 +59,18 @@ export default class AnswerBox extends Component {
             <div className="answer-container">
                 <div className="row">
                     <div className="col-md-6">
-                        <div className="hex-option border" item={this.props.answers[0]} onClick={this.handleClick} dangerouslySetInnerHTML={{ __html: this.props.answers[0].value }} />
+                        <div className="hex-option border" value={this.props.answers[0].value} onClick={this.handleClick} dangerouslySetInnerHTML={{ __html: this.props.answers[0].value }} />
                     </div>
                     <div className="col-md-6">    
-                        <div className="hex-option border" item={this.props.answers[1]} onClick={this.handleClick} dangerouslySetInnerHTML={{ __html: this.props.answers[1].value }} />
+                        <div className="hex-option border" value={this.props.answers[1].value} onClick={this.handleClick} dangerouslySetInnerHTML={{ __html: this.props.answers[1].value }} />
                     </div>
                 </div>
                 <div className="row">
                     <div className="col-md-6">
-                        <div className="hex-option border" item={this.props.answers[2]} onClick={this.handleClick} dangerouslySetInnerHTML={{ __html: this.props.answers[2].value }} />
+                        <div className="hex-option border" value={this.props.answers[2].value} onClick={this.handleClick} dangerouslySetInnerHTML={{ __html: this.props.answers[2].value }} />
                     </div>
                     <div className="col-md-6">
-                        <div className="hex-option border" item={this.props.answers[3]} onClick={this.handleClick} dangerouslySetInnerHTML={{ __html: this.props.answers[3].value }} />
+                        <div className="hex-option border" value={this.props.answers[3].value} onClick={this.handleClick} dangerouslySetInnerHTML={{ __html: this.props.answers[3].value }} />
                     </div>
                 </div>
             </div>
--- src/Game.js
+++ src/Game.js
@@ -60,20 +60,23 @@ export default class Game extends Component {
         if (this.state.questions) {
             return (<QuestionBox item={this.showQuestion()} handleAnswer={this.handleAnswer} />)
         } else {
-            <div class="sk-circle">
-                <div class="sk-circle1 sk-child"></div>
-                <div class="sk-circle2 sk-child"></div>
-                <div class="sk-circle3 sk-child"></div>
-                <div class="sk-circle4 sk-child"></div>
-                <div class="sk-circle5 sk-child"></div>
-                <div class="sk-circle6 sk-child"></div>
-                <div class="sk-circle7 sk-child"></div>
-                <div class="sk-circle8 sk-child"></div>
-                <div class="sk-circle9 sk-child"></div>
-                <div class="sk-circle10 sk-child"></div>
-                <div class="sk-circle11 sk-child"></div>
-                <div class="sk-circle12 sk-child"></div>
+            return (
+            <div className="sk-circle">
+                <div className="sk-circle1 sk-child"></div>
+                <div className="sk-circle2 sk-child"></div>
+                <div className="sk-circle3 sk-child"></div>
+                <div className="sk-circle4 sk-child"></div>
+                <div className="sk-circle5 sk-child"></div>
+                <div className="sk-circle6 sk-child"></div>
+                <div className="sk-circle7 sk-child"></div>
+                <div className="sk-circle8 sk-child"></div>
+                <div className="sk-circle9 sk-child"></div>
+                <div className="sk-circle10 sk-child"></div>
+                <div className="sk-circle11 sk-child"></div>
+                <div className="sk-circle12 sk-child"></div>
             </div>
+
+            )
         }
     }
 }
